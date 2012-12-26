66d6941eaa22d1805d7fc617df7cf60118a0c164
Lucas Videla <videla.lucas@gmail.com>
2012-12-26 17:08:35 -0300

    Se agrega test de la consigna

---
+++ src/test/java/edu/tallerweb/cuentas/CuentaTests.java
@@ -0,0 +1,32 @@
+package edu.tallerweb.cuentas;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CuentaTests {
+
+	@Test
+	public void queVerifiqueLaConsigna() {
+		CuentaSueldo cuenta = new CuentaSueldo();
+		cuenta.depositar(4000.0);
+
+		Assert.assertEquals(
+				"al depositar $ 4000.0 en una cuenta vacía, tiene $ 4000.0",
+				4000.0, cuenta.getSaldo(), 0.0);
+
+		cuenta.extraer(500.0);
+		
+		Assert.assertEquals(
+				"al extraer $ 500.0 de una cuenta con $ 4000.0 se obtienen $ 3500.0",
+				3500.0, cuenta.getSaldo(), 0.0);
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public void queVerifiqueLaConsignaException() {
+		CuentaSueldo cuenta = new CuentaSueldo();
+		cuenta.depositar(3500.0);
+
+		cuenta.extraer(4000.0);
+	}
+	
+}
